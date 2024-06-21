be521fbbcf1fab8aae2786aed759eaedcf9582b1
mcorreira <megancorreiradev@gmail.com>
2024-06-20 22:59:12 -0400

    Initial commit of frontend and backend

---
+++ server/app.js
@@ -0,0 +1,32 @@
+var createError = require('http-errors');
+var express = require('express');
+const path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+const port = process.env.PORT || 5000;
+// view engine setup changed from jade to pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/api', (req, res) => {
+  console.log(req.path);
+  res.json({ message: 'Hello from server!' });
+});
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
+module.exports = app;
