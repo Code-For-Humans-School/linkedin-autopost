dd360d3bb81c4dac08a9309a5f76da07e4a652fd
Ilya Dorman <whitestar.i8@gmail.com>
2024-08-15 17:14:40 +0100

    restored index.js

---
+++ server/index.js
@@ -0,0 +1,61 @@
+require('dotenv').config(); // Load environmental variables from .env file
+var createError = require('http-errors');
+var express = require('express');
+const path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+// Routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+
+// Session storage
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+
+var app = express();
+const port = process.env.PORT || 5000;
+
+// MySQL session store configuration
+const sessionStore = new MySQLStore({
+  host: '127.0.0.1',
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Use session middleware
+app.use(session({
+  key: 'temp_data',
+  secret: process.env.SESSION_SECRET,
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 1 day
+}));
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Set extended to true to handle complex data structures
+
+// view engine setup changed from jade to pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(cors());
+app.use(logger('dev'));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
